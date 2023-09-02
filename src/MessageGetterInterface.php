56d72773aa09a024206ac132de7cdf7a844b1d75
Eric Fortmeyer <efortmeyer@bethel.jw.org>
2023-09-02 10:02:41 -0500

    docs: use php contrib in license

--- src/MessageGetterInterface.php
+++ src/MessageGetterInterface.php
@@ -4,17 +4,13 @@ declare(strict_types=1);
 
 namespace PhpContrib\Validator;
 
-use Stringable;
-
 /**
  * Unifies validation configuration objects with a standard api.
  */
 interface MessageGetterInterface
 {
-  /**
-   * Provides validation messages.
-   *
-   * @return (string|Stringable)[]
-   */
-  public function getMessages(): array;
+    /**
+     * Provides a validation message.
+     */
+    public function getMessage(): string;
 }
